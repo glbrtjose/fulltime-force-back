d751030fa22b3e6b1f8cc809ca87f64517b5290a
glbrtjose <gilbertocarrizo@gmail.com>
2024-07-30 17:54:19 -0400

    Update index.js to change test endpoint from '/test' to '/', and update vercel.json to build and route to index.js instead of app.js

--- index.js
+++ index.js
@@ -11,9 +11,9 @@ console.log('process.env.PORT: ', process.env.PORT);
 //Looking for a local port to host our web application
 const port = process.env.PORT || 4000;
 // Create test endpoint
-app.get("/test", async (req, res) => {
+app.get("/", async (req, res) => {
   res.status(200).json({ message: "Endpoint responding" });
 });
 //Initialize our web-app on the selected port
 app.listen(port, () => {});
-console.log(`Example app listening at http://localhost: ${port}`);
+console.log(`Example app listening at http://localhost:${port}`);
