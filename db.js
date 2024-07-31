3c01fe55f968add8a286049c19ee7c96942cd562
glbrtjose <gilbertocarrizo@gmail.com>
2024-07-31 07:30:46 -0400

    Updated db.js: added quotes around require statements, reformatted mongoose.connect() call, and added quotes around console.log/output strings.

--- db.js
+++ db.js
@@ -1,16 +1,17 @@
 // Import dotenv to load environment variables
-require('dotenv').config();
-const mongoose = require('mongoose');
+require("dotenv").config();
+const mongoose = require("mongoose");
 
 const MONGO_URI = process.env.MONGODB_URI;
 
-mongoose.connect(MONGO_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-})
+mongoose
+  .connect(MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
   .then(() => {
-    console.log('Connected to MongoDB');
+    console.log("Connected to MongoDB");
   })
   .catch((error) => {
-    console.error('Error connecting to MongoDB:', error);
+    console.error("Error connecting to MongoDB:", error);
   });
